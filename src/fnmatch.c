fda2732fc002f1ccdca8283b10810dba3547e63c
Charles Cabergs <me@cacharle.xyz>
2022-01-20 23:03:00 +0100

    Added fnmatch extensions draft

--- src/fnmatch.c
+++ src/fnmatch.c
@@ -9,11 +9,45 @@ _fnmatch(const char *pattern, const char *string, int flags)
         return *string == '\0';
     if (*string == '\0')
         return strcmp(pattern, "*") == 0;
-    if (*pattern == '*')
+    if (flags & GLBL_FNM_PATHNAME && *string == '/')
     {
-        if (_fnmatch(pattern + 1, string, flags))
-            return true;
-        return _fnmatch(pattern, string + 1, flags);
+        if (*pattern == *string)
+        {
+            if (flags & _GLBL_FNM_PERIOD_PATH && string[1] != '.' && pattern[1] != '.')
+                return false;
+            return _fnmatch(pattern + 1, string + 1, flags);
+        }
+        return false;
+    }
+    if (flags & GLBL_FNM_PERIOD && *string == '.')
+    {
+        if (*pattern == *string)
+            return _fnmatch(pattern + 1, string + 1, flags & ~GLBL_FNM_PERIOD | _GLBL_FNM_PERIOD_PATH);
+        return false;
+    }
+    switch (*pattern)
+    {
+        case '\\':
+            if (!(flags & GLBL_FNM_NOESCAPE))
+                pattern++;
+            break;
+        case '*':
+            if (_fnmatch(pattern + 1, string, flags))
+                return true;
+            return _fnmatch(pattern, string + 1, flags);
+        case: '?':
+            return _fnmatch(pattern + 1, string + 1, flags);
+        case '[':
+            pattern++;
+            if (*pattern == *string) // has to contain at least one character
+                return _fnmatch(strchr(pattern, ']') + 1, string + 1, flags);
+            pattern++;
+            for (; *pattern != ']'; pattern++)
+            {
+                if (*pattern == *string)
+                    return _fnmatch(strchr(pattern, ']') + 1, string + 1, flags);
+            }
+            return false;
     }
     if (*pattern == *string)
         return _fnmatch(pattern + 1, string + 1, flags);
@@ -23,6 +57,7 @@ _fnmatch(const char *pattern, const char *string, int flags)
 int
 glbl_fnmatch(const char *pattern, const char *string, int flags)
 {
+    // TODO: check for [ ] error before running fnmatch
     return _fnmatch(pattern, string, flags) ?
         GLBL_FNM_MATCH : GLBL_FNM_NOMATCH;
 }
