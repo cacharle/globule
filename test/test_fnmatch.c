0c91840d7728ac04f1f77737b0a824a46dd407f6
Charles Cabergs <me@cacharle.xyz>
2022-01-20 22:42:44 +0100

    Added wildcard tests

--- test/test_fnmatch.c
+++ test/test_fnmatch.c
@@ -9,3 +9,16 @@ Test(glbl_fnmatch, string_comparison)
     cr_assert_eq(glbl_fnmatch("ab", "abc", 0), GLBL_FNM_NOMATCH);
     cr_assert_eq(glbl_fnmatch("a", "", 0), GLBL_FNM_NOMATCH);
 }
+
+Test(glbl_fnmatch, wildcard)
+{
+    cr_assert_eq(glbl_fnmatch("*", "", 0), GLBL_FNM_MATCH);
+    cr_assert_eq(glbl_fnmatch("*", "a", 0), GLBL_FNM_MATCH);
+    cr_assert_eq(glbl_fnmatch("*", "abcdefgh", 0), GLBL_FNM_MATCH);
+    cr_assert_eq(glbl_fnmatch("a*", "abcdefgh", 0), GLBL_FNM_MATCH);
+    cr_assert_eq(glbl_fnmatch("*h", "abcdefgh", 0), GLBL_FNM_MATCH);
+    cr_assert_eq(glbl_fnmatch("a*h", "abcdefgh", 0), GLBL_FNM_MATCH);
+    cr_assert_eq(glbl_fnmatch("a*z", "abcdefgh", 0), GLBL_FNM_NOMATCH);
+    cr_assert_eq(glbl_fnmatch("a*b*c", "aIIIbIIIc", 0), GLBL_FNM_MATCH);
+    cr_assert_eq(glbl_fnmatch("a*b*c", "aIIIIIIc", 0), GLBL_FNM_NOMATCH);
+}
